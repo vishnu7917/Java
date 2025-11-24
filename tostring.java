8ef753012a1406df89eb77cd7e9d88b521373acb
Vishnu Vardhan Reddy <103874448+vishnu7917@users.noreply.github.com>
2025-11-24 13:36:48 +0530

    Add files via upload

---
+++ tostring.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class tostring {
+    public static void main(String[] args) {
+        String str;
+        System.out.println("ENter a String:");
+        Scanner sobj=new Scanner(System.in);
+        str=sobj.nextLine();
+        String str1;
+        str1 = "vishnu";
+        System.out.println(str.equals(str1));
+    }
+}
